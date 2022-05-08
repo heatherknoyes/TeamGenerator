ae0a157fa9a4732dbf10463d78df6c14f7e35daf
Heather Noyes <heatherknoyes@gmail.com>
2022-05-08 09:11:14 -0400

    Created initial command line interaction to output a generic html

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const generateTeam = require("./utils/generateTeam");
 
-// need to ask the user to input details for a team member
-// need to loop until they are done inputting team members
-// for each team member a new team member card would need to be created
-let overallHtml = ``;
+const confirmAnswerValidator = (answer) => {
+  if (answer !== "") {
+    return true;
+  }
+  return "Please enter at least one character.";
+};
+
+const questions = [
+  {
+    type: "input",
+    message: "Team Member Name: ",
+    name: "title",
+    validate: confirmAnswerValidator,
+  },
+  {
+    type: "input",
+    message: "Team Member ID: ",
+    name: "id",
+    validate: confirmAnswerValidator,
+  },
+  {
+    type: "input",
+    message: "Email address: ",
+    name: "email",
+    validate: confirmAnswerValidator,
+  },
+  {
+    type: "list",
+    message: "Employee Type ",
+    name: "employeeType",
+    choices: ["Employee", "Manager", "Intern", "Engineer"],
+  },
+];
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, generateTeam(data), (err) =>
+    err ? console.log(err) : console.log("Success!")
+  );
+}
+
+function init() {
+  inquirer
+    .prompt(questions)
+    .then((response) => {
+      const filename = "./dist/index2.html";
+      writeToFile(filename, response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+init();
